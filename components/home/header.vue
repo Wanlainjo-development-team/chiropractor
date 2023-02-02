bde72e57de722b29dd73738d71097fe4a11b2109
rukkiecodes <rukkiecodes@gmail.com>
2023-02-02 04:39:52 +0100

    dont laying out section 1

--- components/home/header.vue
+++ components/home/header.vue
@@ -6,40 +6,42 @@
     cycle
     interval="20000"
   >
-    <v-carousel-item
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item"
-      lazy-src="https://picsum.photos/id/11/100/60"
-      cover
-      gradient="to bottom, rgba(0,0,0,.6), rgba(0,0,0,.6)"
-    >
-      <v-container class="pa-0">
-        <v-card
-          height="500"
-          max-width="100%"
-          color="transparent"
-          class="d-flex align-center px-2 px-sm-16"
-          flat
-        >
-          <v-card-text class="white--text d-flex flex-column align-start">
-            <span
-              class="text-h5 text-sm-h4 text-lg-h3 text-white font-weight-bold"
-              >Our Team Welcomes You</span
-            >
-            <span class="text-white text-lg-body-1"
-              >chiropractic and wellness clinic</span
-            >
-            <v-btn
-              color="blue"
-              flat
-              class="mt-4 text-body-2 font-weight-bold text-capitalize"
-              to="/contact"
-              >Make an appointment</v-btn
-            >
-          </v-card-text>
-        </v-card>
-      </v-container>
+    <v-carousel-item v-for="(item, i) in items" :key="i">
+      <v-parallax
+        :src="item"
+        lazy-src="https://res.cloudinary.com/rukkiecodes/image/upload/v1675307350/chriopractor/placeholder_lnnfrn.jpg"
+        gradient="to bottom, rgba(0,0,0,.6), rgba(0,0,0,.6)"
+      >
+        <v-container class="pa-0">
+          <v-card
+            height="500"
+            max-width="100%"
+            color="transparent"
+            class="d-flex align-center"
+            flat
+          >
+            <v-card-text class="white--text d-flex flex-column align-start">
+              <span
+                class="
+                  text-h5 text-sm-h4 text-lg-h3 text-white
+                  font-weight-bold
+                "
+                >Our Team Welcomes You</span
+              >
+              <span class="text-white text-lg-body-1"
+                >chiropractic and wellness clinic</span
+              >
+              <v-btn
+                color="blue"
+                flat
+                class="mt-4 text-body-2 font-weight-bold text-capitalize"
+                to="/contact"
+                >Make an appointment</v-btn
+              >
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-parallax>
     </v-carousel-item>
   </v-carousel>
 </template>
