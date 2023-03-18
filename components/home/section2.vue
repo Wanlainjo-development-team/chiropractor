93d2c42f431153973598649cb05f60237477efaa
rukkiecodes <rukkiecodes@gmail.com>
2023-03-19 00:10:01 +0100

    setup home page again

--- components/home/section2.vue
+++ components/home/section2.vue
@@ -1,127 +1,58 @@
 <template>
-  <v-parallax
-    src="https://res.cloudinary.com/rukkiecodes/image/upload/v1675309154/chriopractor/logo-circle-bg_oudvpl.png"
-    class="d-flex align-center justify-center"
-  >
-    <v-container class="mt-16 pt-0">
-      <v-row justify="space-between">
-        <v-col cols="12" sm="5">
-          <v-card flat class="pa-0 ma-0" color="transparent">
-            <v-img
-              cover
-              class="pa-0 ma-0 d-flex align-center text-center"
-              src="https://res.cloudinary.com/rukkiecodes/image/upload/v1675310178/chriopractor/chiropractor-video-thumbnail_a6kjzz.png"
-            >
-              <v-btn icon color="blue" size="large" elevation="10">
-                <v-icon>mdi-play</v-icon>
-
-                <!-- <v-dialog
-                  v-model="dialog"
-                  activator="parent"
-                  width="500"
-                  height="500"
-                  scrollable
-                >
-                  <v-card height="500">
-                    <v-card-text class="ma-0 pa-0" style="max-height: 600px">
-                      <iframe
-                        width="100%"
-                        height="100%"
-                        src="https://www.youtube.com/embed/cplYgrvLrks"
-                        title="Welcome to Metronet Chiropractic Clinic.  Chiropractic, Acupuncture, and Natural Healthcare."
-                        frameborder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
-                      ></iframe>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-btn class="bg-blue" block text @click="dialog = false">
-                        Close
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog> -->
-              </v-btn>
-            </v-img>
-          </v-card>
-        </v-col>
-        <v-col cols="12" sm="7">
-          <v-card flat color="transparent">
-            <v-card-subtitle class="text-grey-darken-3"
-              >Metronet Chiropractic Clinic</v-card-subtitle
-            >
-            <v-card-title class="font-weight-bold text-grey-darken-3"
-              >Welcome to Wellness</v-card-title
-            >
-            <v-card-text class="text-body-2 text-grey-darken-3">
-              Welcome to Metronet Chiropractic, the premier chiropractic clinic
-              serving the Twin Cities metro area. Led by Dr. Chris Fokumlah, our
-              team of experienced professionals is dedicated to providing
-              exceptional care to patients of all ages, with a special focus on
-              those who have been injured in automobile accidents, work-related
-              accidents, and sports injuries. Dr. Chris is a highly trained and
-              skilled chiropractor who uses a range of techniques to address
-              acute and chronic injuries, as well as to promote overall
-              wellness.
-            </v-card-text>
-            <v-card-text class="pt-0 text-body-2 text-grey-darken-3">
-              With a background in microbiology and functional medicine
-              training, Dr. Chris brings a scientific and holistic approach to
-              his chiropractic practice, with a commitment to resolving symptoms
-              and promoting long-term healing. We are passionate about the
-              health and wellbeing of our patients, and we look forward to
-              helping you achieve optimal health and vitality.
-            </v-card-text>
-          </v-card>
-          <v-card flat color="transparent">
-            <v-card-title class="font-weight-bold text-grey-darken-3"
-              >Metronet Chiropractic Clinic Treatments</v-card-title
-            >
-            <v-card-text>
-              <v-row justify="space-between" dense>
-                <v-col
-                  v-for="(link, i) in links"
-                  :key="i"
-                  cols="12"
-                  sm="6"
-                  class="d-flex align-start"
-                >
-                  <v-icon size="18" color="blue" class="mr-2"
-                    >mdi-arrow-right-bold-circle</v-icon
-                  >
-                  <v-hover>
-                    <template v-slot:default="{ isHovering, props }">
-                      <span
-                        v-bind="props"
-                        class="font-weight-bold"
-                        :class="isHovering ? 'text-blue' : 'text-grey-darken-3'"
-                        >{{ link.title }}</span
-                      >
-                    </template>
-                  </v-hover>
-                </v-col>
-              </v-row>
-            </v-card-text>
-            <v-card-actions>
-              <div class="d-flex align-center justify-sart">
-                <v-icon color="blue" size="30">mdi-calendar-month</v-icon>
-                <span class="text-h6 text-grey-darken-3"
-                  >Book an appointment</span
-                >
-              </div>
-              <v-spacer />
-              <div class="d-flex align-center justify-sart">
-                <v-icon color="blue" size="30">mdi-star</v-icon>
-                <span class="text-h6 text-grey-darken-3"
-                  >New patients Offer!</span
-                >
-              </div>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-parallax>
+  <v-container class="mt-16 pt-0">
+    <v-row justify="space-between">
+      <v-col cols="12" sm="5">
+        <v-card flat class="pa-0 ma-0 rounded-0" color="transparent">
+          <v-img cover class="pa-0 ma-0 d-flex align-center text-center"
+            src="https://res.cloudinary.com/rukkiecodes/image/upload/v1675876242/chriopractor/Chiropractors-TheBrost-Wayzata-011-2880w_hx3r1r.jpg" />
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="7">
+        <v-card flat color="transparent">
+          <v-card-title class="font-weight-bold text-grey-darken-3">Welcome to Metronet Chiropractic Clinic</v-card-title>
+          <v-card-text class="text-grey-darken-3">For more information or to schedule an appointment with Dr. Chris
+            Fokumlah,<br> call <a href="tel:+1-612-296-5143" class="text-grey-darken-3">+1-612-296-5143</a>
+            today!</v-card-text>
+          <v-card-text class="text-body-2 text-grey-darken-3">
+            Welcome to Metronet Chiropractic, the premier chiropractic clinic
+            serving the Twin Cities metro area. Led by Dr. Chris Fokumlah, our
+            team of experienced professionals is dedicated to providing
+            exceptional care to patients of all ages, with a special focus on
+            those who have been injured in automobile accidents, work-related
+            accidents, and sports injuries. Dr. Chris is a highly trained and
+            skilled chiropractor who uses a range of techniques to address
+            acute and chronic injuries, as well as to promote overall
+            wellness.
+          </v-card-text>
+          <v-card-text class="pt-0 text-body-2 text-grey-darken-3">
+            With a background in microbiology and functional medicine
+            training, Dr. Chris brings a scientific and holistic approach to
+            his chiropractic practice, with a commitment to resolving symptoms
+            and promoting long-term healing. We are passionate about the
+            health and wellbeing of our patients, and we look forward to
+            helping you achieve optimal health and vitality.
+          </v-card-text>
+        </v-card>
+        <v-card flat color="transparent">
+          <v-card-title class="font-weight-bold text-grey-darken-3">Metronet Chiropractic Clinic
+            Treatments</v-card-title>
+          <v-card-text>
+            <v-row justify="space-between" dense>
+              <v-col v-for="(link, i) in links" :key="i" cols="12" sm="6" class="d-flex align-start">
+                <v-icon size="16" color="blue" class="mr-2">mdi-arrow-right-bold-circle</v-icon>
+                <v-hover>
+                  <template v-slot:default="{ isHovering, props }">
+                    <span v-bind="props" class="font-weight-bold text-body-2"
+                      :class="isHovering ? 'text-blue' : 'text-grey-darken-3'">{{ link.title }}</span>
+                  </template>
+                </v-hover>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
