9883b6314322d4b31c8369f1193e7f1d3bf4da55
rukkiecodes <rukkiecodes@gmail.com>
2023-05-17 19:46:58 +0100

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,15 @@ export default defineNuxtConfig({
         // ...
         '@pinia/nuxt',
     ],
+
+    app: {
+        head: {
+            title: 'Your Custom Title',
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ]
+        },
+    }
 })
